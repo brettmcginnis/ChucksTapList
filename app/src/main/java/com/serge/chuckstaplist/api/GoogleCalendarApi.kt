ddb471a2684a4ba61e7068be723c26c1b389b68b
Serge Chernetskiy <serge@chernetskiy.dev>
2022-08-15 23:40:56 -0700

    Fix calendar api migration issue

--- app/src/main/java/com/serge/chuckstaplist/api/GoogleCalendarApi.kt
+++ app/src/main/java/com/serge/chuckstaplist/api/GoogleCalendarApi.kt
@@ -3,7 +3,7 @@ package com.serge.chuckstaplist.api
 import com.serge.chuckstaplist.api.calendar.CalendarDto
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.request.post
+import io.ktor.client.request.get
 import org.koin.core.annotation.Factory
 
 private const val BASE_URL = "https://www.googleapis.com/calendar/v3/calendars"
@@ -11,5 +11,5 @@ private const val BASE_URL = "https://www.googleapis.com/calendar/v3/calendars"
 @Factory
 class GoogleCalendarApi(private val client: HttpClient) {
     suspend fun getCalendar(calendarId: String, apiKey: String, timeMin: String, timeMax: String): CalendarDto =
-        client.post("$BASE_URL/$calendarId/events?singleEvents=true&key=$apiKey&timeMin=$timeMin&timeMax=$timeMax").body()
+        client.get("$BASE_URL/$calendarId/events?singleEvents=true&key=$apiKey&timeMin=$timeMin&timeMax=$timeMax").body()
 }
